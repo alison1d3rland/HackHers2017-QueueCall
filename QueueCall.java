ba39e33b908c1ac34b33182b08d2308c83b10624
Alison Thi <alisothi@gmail.com>
2017-02-18 16:48:36 -0500

    Update QueueCall.java

--- QueueCall.java
+++ QueueCall.java
@@ -1,6 +1,25 @@
 public class QueueCall{
 
+	private String[] queueArray;
+	private int queueSize;
+	private int front, rear, numberOfPeople=0;
 	
+	QueueCall(int size){
+		queueSize = size;
+		queueArray = new String(size);
+		Array.fill(queueArray, "-1");
+	}
 
+	public void insert(String input){
+		if(numberOfPeople+1 <= queueSize){
+			queueArray[rear] = input;
+			rear++;
+		}
+
+	}
+	
+	public static void main(String[] args){
+		
+	}
 
 }
